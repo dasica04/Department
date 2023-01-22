e8c08e9b18371414aa706eba215d32043e1ed9c7
dasica04 <dasica.dewangan@ssipmt.com>
2023-01-22 21:11:52 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php include("header.php");?>
+<?php include('db_con.php');?>
+<div class="container-fluid">
+    <div class="row" id="wrapper">
+    <?php require_once'menubar.php';?>
+    <div class="col-sm-12">
+        <div class="panel panel-default">
+        <div class="panel-body">
+
+<center><h1>Shri Shankaracharya Institute of Professional Management and Technology, Raipur</h1>
+		<b>Founded on: 2008</b></center>
+    <hr>
+
+    	<div class="col-sm-6">
+    		<b>Degree offers with duration:</b>
+    		<ul>
+    			<li>Under Graduate Course (B.Tech)</li>
+				<li>Post Graduate Course (Master in Business Administration)</li>
+				<li>Post Graduate Courses (M.Tech)</li>
+				<li>Ph.D</li>
+    		</ul>
+			<br>
+				<br>
+			<b>Resources:</b>
+			<ul>
+				<li>Number of faculty members: 50</li>
+				<li>Number of technical staff: 20</li>
+				<li>Internet: Department wide internet service with Wi-Fi Support</li>
+				<li>Teaching-Learning facilities</li><br>
+				<li><b>Undergraduate Laboratories:</b><br>
+					Computer Lab, Network Lab
+					DBMS Lab, Communications Lab
+					Electronics & Digital Systems Lab
+					Microprocessor Lab
+					Image Processing Lab
+					Signal Processing Lab
+					Software Engineering Lab</li>
+			</ul>
+			<br>
+				<br>
+			<b>Admission:</b><br>
+			<p>Admission process starts in early July of each year. The local student will be notified through daily newspaper. The International student have to apply through Bangladeshi mission located in their country, for further information visit www.dept.ru.ac.bd/cse or email us cse@dept.ru.ac.bd</p>
+    	</div>
+    	<div class="col-sm-6">
+    		<b>Research Groups:</b><br>
+			<ul>
+				<li>Signal Processing and Neuoro Science</li>
+				<li>Embedded Systems and Robotics</li>
+				<li>Multimedia Processing</li>
+				<li>Intelligent Computing</li>
+				<li>Network Computing</li>
+				<li>Seminar Library: More than 2500 titles with Internet facilities.</li>
+			</ul>
+			<p> -- All labs and class rooms are equippedwith audio-visual supports</p>
+			<br>
+			<b>Extra-curriculum activities:</b><br>
+			<ul>
+				<li>Programming contest</li>
+				<li>Project development contest</li>
+				<li>Cultural program, debating and sports</li>
+				<li>National wide achievements in programming contest and project</li>
+				<li>development</li>
+			</ul>
+			
+    	</div>
+
+		</div>
+		</div>
+	</div>
+	</div>
+</div>
---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php include("header.php");?>
+<?php include('db_con.php');?>
+<div class="container-fluid">
+    <div class="row" id="wrapper">
+    <?php require_once'menubar.php';?>
+    <div class="col-sm-12">
+        <div class="panel panel-default">
+        <div class="panel-body">
+
+<center><h1>Department of Information Technology - SSIPMT, Raipur</h1></center>
+    <hr>
+
+<div class="col-sm-4 col-sm-offset-4" style="height:430px;">
+        <p> Old Dhamtari Road,<br>
+       P.O. : Sejabahar, Mujgahan<br>
+       Pin Code: 492015, Raipur, Chhattisgarh<br>
+       Phone (Reception) : 0771-3501600 / 01<br>
+       Registrar: 0771-3501602<br>
+       Accounts: 0771-3501603<br>
+       E-Mail: info@ssipmt.com<br>
+        </p>
+
+</div>
+		</div>
+		</div>
+	</div>
+	</div>
+</div>
